93010c81f41187c03aff42768d4785171d7ba72f
shafikshaon <shafikshaon@gmail.com>
2017-11-29 22:22:23 +0600

    js file chnage

--- resources/views/layouts/back/script.blade.php
+++ resources/views/layouts/back/script.blade.php
@@ -28,5 +28,18 @@ $(document).ready(function(){
 
     });
 
+    $('.i-checks').iCheck({
+        checkboxClass: 'icheckbox_square-green',
+        radioClass: 'iradio_square-green',
+    });
+
+    $('#data_1 .input-group.date').datepicker({
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true
+    });
+
 });
 </script>
