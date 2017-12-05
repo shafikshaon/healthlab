de9abc707c92113f2afb49afc46bb861b9828050
shafikshaon <shafikshaon@gmail.com>
2017-12-05 10:53:35 +0600

    Route file modify

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,29 @@ Route::post('register', ['as' =>'registernewuser', 'uses' => 'AccountController@
 Route::post('login', ['as' =>'login', 'uses' => 'AccountController@postMyLogin']);
 Route::get('logout', ['as' =>'logout', 'uses' => 'AccountController@getLogout']);
 
-
+//Profile
 Route::get('profile', ['as' =>'adminprofile', 'uses' => 'AccountController@getAdminProfile']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 Route::get('diagnosis', ['as' =>'diagnosis', 'uses' => 'DiagnosisController@getMyDiagnosis']);
@@ -42,28 +63,49 @@ Route::prefix('admin/admin')->group(function () {
 //Doctors
 Route::prefix('admin/doctor')->group(function () {
   Route::get('viewall', ['as' =>'viewalldoctor', 'uses' => 'DoctorCrudController@getAllDoctors']);
-
   Route::get('add', ['as' =>'adddoctor', 'uses' => 'DoctorCrudController@getAddDoctor']);
-
   Route::post('insert', ['as' =>'insertdoctor', 'uses' => 'DoctorCrudController@postInsertDoctor']);
-
   Route::get('edit/{id}', ['as' =>'editdoctor', 'uses' => 'DoctorCrudController@getSingleDoctor']);
-
   Route::post('update/{id}', ['as' =>'updatedoctor', 'uses' => 'DoctorCrudController@updateSingleDoctor']);
-
   Route::get('delete/{id}', ['as' =>'deletedoctor', 'uses' => 'DoctorCrudController@deleteSingleDoctor']);
-
   Route::get('read/{id}', ['as' =>'readdoctor', 'uses' => 'DoctorCrudController@readSingleDoctor']);
 });
 
 //Patients
 Route::prefix('admin/patient')->group(function () {
   Route::get('viewall', ['as' =>'viewallpatient', 'uses' => 'PatientCrudController@getAllPatients']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 //Developers
 Route::prefix('admin/developer')->group(function () {
   Route::get('viewall', ['as' =>'viewalldeveloper', 'uses' => 'DeveloperCrudController@getAllDevelopers']);
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 //Organs
